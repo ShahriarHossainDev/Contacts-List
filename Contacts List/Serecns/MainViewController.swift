b2600d91bf44f5a301800f75d981772daa8dd52e
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-03-19 10:49:05 +0600

    MVVM

--- Contacts List/Serecns/MainViewController.swift
+++ Contacts List/Serecns/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Contacts List
 //
 //  Created by Shishir_Mac on 19/3/23.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
